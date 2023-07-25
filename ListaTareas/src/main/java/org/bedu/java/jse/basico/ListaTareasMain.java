e1feadb4ada57d5d0931267609038e79f7a7e7f5
Hugo Munguia <hugruby@gmail.com>
2023-07-24 22:50:18 -0600

    fix! seguimiento a proyectos

---
+++ ListaTareas/src/main/java/org/bedu/java/jse/basico/ListaTareasMain.java
@@ -0,0 +1,11 @@
+package org.bedu.java.jse.basico;
+
+public class ListaTareasMain {
+    public static void main(String[] args) {
+        System.out.println("1. Crear nueva lista de tareas\n" +
+                "2. Ver listas de tareas\n" +
+                "3. Ver tareas de lista\n" +
+                "4. Actualizar lista de tareas.\n" +
+                "5. Eliminar lista de tarea");
+    }
+}
